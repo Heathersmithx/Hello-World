a144e5051f270bd0d8fea5984539907711c5030b
HeatherSmithx <Heathersmithx@gmail.com>
2017-08-21 20:23:53 -0600

    Uploading Hello World Files

---
+++ HelloWorld/src/HelloWorldTest/helloWorld.java
@@ -0,0 +1,15 @@
+package HelloWorldTest;
+
+import javax.swing.JOptionPane;
+
+public class helloWorld {
+	
+	
+	public static void main (String[] args){
+		
+		System.out.println("Hello Everyone in the world!");
+		JOptionPane.showMessageDialog(null, "Hello Everyone again!");
+		
+	}
+
+}
